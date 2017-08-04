7d92d4c066f76af2828269f223d72b3e69418301
Harmanpreet Singh <Harmanpreet.preetsingh@gmail.com>
2017-08-04 15:46:50 -0400

    Added Project Files

---
+++ COMP123-S2017-Assign05/COMP123-S2017-Assign05/BMICalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Harmanpreet Singh
+ * StudentID : 300931446
+ * Date: August 4,2017
+ * Description: BMI Calculator
+ * Version 0.1: Created Project Files
+ */
+namespace COMP123_S2017_Assign05
+{
+    public partial class BMICalculator : Form
+    {
+        public BMICalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ COMP123-S2017-Assign05/COMP123-S2017-Assign05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Harmanpreet Singh
+ * StudentID : 300931446
+ * Date: August 4,2017
+ * Description: BMI Calculator
+ * Version 0.1: Created Project Files
+ */
+namespace COMP123_S2017_Assign05
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BMICalculator());
+        }
+    }
+}
